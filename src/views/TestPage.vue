d83ace5ab9bae36f03ec2da2e07b2e17387082a3
risoyo <yinglongnvba@gmail.com>
2020-03-01 20:47:23 +0800

    添加测试页

--- src/views/TestPage.vue
+++ src/views/TestPage.vue
@@ -6,6 +6,9 @@
     <el-button @click="click2">
       获取cookie
     </el-button>
+    <el-button @click="click3">
+      testRequireToken
+    </el-button>
   </div>
 </template>
 
@@ -21,6 +24,15 @@ export default {
     click2() {
       const token = this.getCookie('token'); // 获取Cookie
       console.log(token);
+    },
+    click3() {
+      this.get('/community/alpha/testRequireToken')
+        .then(resp => {
+          console.log('resp' + JSON.stringify(resp));
+        })
+        .catch(e => {
+          console.log('Vue GET error catched :' + e);
+        });
     }
   }
 };
